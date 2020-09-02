56322c2bfb091bb04969ae4c98a77d250042f044
matsarat <mat.saratowicz@gmail.com>
2020-09-02 14:35:57 +0200

    Added code that returns table of integers

--- data_reader.py
+++ data_reader.py
@@ -1,8 +1,11 @@
 class DataReader(object):
     def __init__(self, path_to_file):
         file = open(path_to_file)
-        self.lines = [line.strip() for line in file.readlines()]
+        lines = file.readlines()
         file.close()
+        self.lines = []
+        for line in lines:
+            self.lines.append(line.strip())
 
 
     def get_table(self, separator):
@@ -13,3 +16,10 @@ class DataReader(object):
             table.append(columns)
         return table
 
+    def get_table_int(self, separator):
+        table = []
+        for index in range(1, len(self.lines)):
+            line = self.lines[index]
+            columns = line.split(separator)
+            table.append(list(map(int, columns)))
+        return table
