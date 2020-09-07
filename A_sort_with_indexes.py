d2351464e899a6b605a3035b52a404a4ad66e8e5
matsarat <mat.saratowicz@gmail.com>
2020-09-07 17:41:42 +0200

    Sorts list and prints initial index of list elements
    Rebuilt

--- A_sort_with_indexes.py
+++ A_sort_with_indexes.py
@@ -1,32 +1,46 @@
 from data_reader import DataReader
+
 data_reader = DataReader("sums_in_loop.txt")
 data = data_reader.get_table_int(" ")
 print(data)
 
 
-def swap(first, next):
-    list[index + 1] = first
-    list[index] = next
+class Number(object):
+    def __init__(self, value, initial_index):
+        self.value = value
+        self.initial_position = initial_index + 1
 
-def not_sorted():
-    for index in range(0, len(list)):
-        if index + 1 < len(list):
-            if list[index] > list[index + 1]:
+    def __str__(self):
+        return str(self.initial_position)
+
+    def __repr__(self):
+        return str(self.initial_position)
+
+
+def not_sorted(numbers):
+    for index in range(0, len(numbers)):
+        if index + 1 < len(numbers):
+            if numbers[index].value > numbers[index + 1].value:
                 return True
         else:
             return False
 
-unsorted = []
-for list in data:
-    while not_sorted():
-        for index in range(0, len(list)):
-            if index + 1 < len(list):
-                first = list[index]
-                next = list[index + 1]
-                if first > next:
-                    swap(first, next)
 
-print(*unsorted)
-print(list)
+def swap(numbers, first, next, index):
+    numbers[index + 1] = first
+    numbers[index] = next
 
 
+for lista in data:
+    numbers = []
+    for index in range(0, len(lista)):
+        numbers.append(Number(lista[index], index))
+
+    while not_sorted(numbers):
+        for index in range(0, len(numbers)):
+            if index + 1 < len(numbers):
+                first = numbers[index]
+                next = numbers[index + 1]
+                if first.value > next.value:
+                    swap(numbers, first, next, index)
+    print(numbers)
