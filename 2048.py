ea71f77a307fec3c301b8122126366fcb1d2e545
matsarat <mat.saratowicz@gmail.com>
2020-10-08 22:16:02 +0200

    Correct summation during player's move

--- 2048.py
+++ 2048.py
@@ -125,9 +125,9 @@ def move_down(board):
 
 
 def right_or_down_sum(array):
-    for index in range(0, len(array) - 1):
-        if array[index].value == array[index + 1].value:
-            array[index + 1].value += array[index].value
+    for index in range(len(array) - 1, 0, -1):
+        if array[index].value == array[index - 1].value:
+            array[index - 1].value += array[index].value
             array[index].value = 0
 
 
@@ -144,9 +144,9 @@ def sum_down(board):
 
 
 def left_or_up_sum(array):
-    for index in range (len(array) -1, 0, -1):
-        if array[index].value == array[index - 1].value:
-            array[index -1].value += array[index].value
+    for index in range(0, len(array) - 1):
+        if array[index].value == array[index + 1].value:
+            array[index + 1].value += array[index].value
             array[index].value = 0
 
 
@@ -218,4 +218,4 @@ def game(number_of_rows, number_of_columns):
         place_next_element_on_board(board)
         print_board(board)
 
-game(2, 2)
+game(4, 4)
